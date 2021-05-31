75f416b443b44a403861e8cb36da8e2d84c149c6
skarakas1 <skarakas21@ucla.edu>
2021-05-30 23:33:02 -0700

    Co-authored-by: albertkun <albertkun@users.noreply.github.com>

---
+++ Emily_Project/script/init.js
@@ -0,0 +1,172 @@
+//array for metadata categories
+let metaCat = ["Title","Creator", "Description", "Date Created", "Material", "Dimensions", "Subject", "Language", "Type", "Format", "Rights", "Identifier"];
+
+//-------------------------------------arrays for indv details
+
+let cwolfe_aw_001 = ["\"Award\" Valentine 2013", "Wolfe, Cynthia A.", "Handmade paper Valentine in the shape of an award ribbon. Composed of paper, a metal ring, hemp thread , wallpaper samples, book pages and a vellum envelope" , "2013" , "Paper, Metal and Thread" ,  "6\"x3\"", "collage (technique); mixed media; greeting cards; valentines; cut-paper work" , "English" , "Image" , "JPEG" ,  "All rights reserved." , "Images may not be used without permission of the artist." , "cwolfe-aw-001"];
+
+let cwolfe_aw_002 = ["Valentine 2021", "Wolfe, Cynthia A", "Handmade Valentine's Day card with hand drawn flowers, foliage, and a pink and red embroidered heart on a textured paper background.", "2021", "Ink and Thread on Paper", "9\"x6\"", "collage (technique); mixed media; greeting cards; valentines", "English", "Image", "JPEG", "All rights reserved. Images may not be used without permission of the artist.", "cwolfe-aw-002"];
+
+let cwolfe_aw_003 = ["Valentine 2014" , "Wolfe, Cynthia A." , "Handmade paper Valentine with hand cut paper hearts and foliage with an image of a Victorian era woman on a pink glitter background. The card opens horizontally to reveal a Valentine's Day message." , "2014" , "Paper" , "5.75\"x4.25\”" , "collage (technique); mixed media; greeting cards; valentines; cut-paper work" , "English" , "Image" , "JPEG" , "All rights reserved. Images may not be used without permission of the artist." , "cwolfe-aw-003"];
+
+let cwolfe_aw_004 = ["Valentine's Day Booklet", "Wolfe, Cynthia A.", "Handmade 14 page origami booklet decorated with hand cut hearts and abstract organic shapes. Images come from atlases, art magazines, and dictionaries. The booklet can be viewed horizontally or vertically.","circa 2015","Paper","3\"x4.5\"", "collage (technique); mixed media; greeting cards; valentines, booklets; folded books; cut-paper work", "English", "Image", "JPEG", "All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-004"];
+
+let cwolfe_aw_005 = ["Valentine 2009", "Wolfe, Cynthia A.", "Hand lettered and collaged Valentine's Day card overlayed on a page of French poetry. The main collage component is wrapped in clear cellophane and the card opens vertically to reveal a Valentine's Day message.", "2009", "Paper, Ink, Faux Gemstones, and Plastic" , "5\"x7\"", "collage (technique); mixed media; greeting cards; valentines", "English", "Image", "JPEG", "All rights reserved. Images may not be used without permission of the artist.", "cwolfe-aw-005"];
+
+let cwolfe_aw_006 = ["Pop Up Valentine","Wolfe, Cynthia A.","Pop-up style valentine on a wallpaper sample background, decorated in painted paper cutouts.","circa 2018","Wallpaper, paint, paper, tape","10\"x5.5\”","collage (technique); mixed media; greeting cards; valentines; pop-up books","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-006"];
+
+let cwolfe_aw_007 = ["Valentine 2020","Wolfe, Cynthia A.","Handmade collage valentine on an iridescent paper background decorated with robins and heart cut-outs. Opens vertically to reveal a valentine message.","2020","Paper, wallpaper, paper doily","5.5\"x5\"","collage (technique); mixed media;  greeting cards; ","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-007"];
+
+let cwolfe_aw_008 = ["Abstract Floral Card","Wolfe, Cynthia A.","Digitially drawn and altered abstract florals, printed and adhered to a sturdy, brown paper background with cut scalloped edges.","circa 2017","Paper, digital image","7.75\" x 5.5\"","collage (technique); mixed media;  greeting cards; digital images ","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-008"];
+
+let cwolfe_aw_009 = ["Collaged Postcard 2011","Wolfe, Cynthia A.", "Handmade postcard on thick watercolor paper. Decorated with cutouts from art and fashion books and magazines.","2011","Paper and magazine cutouts","6.75\" x 5\"","collage (technique); mixed media;  greeting cards; postcards","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-009"];
+
+let cwolfe_aw_010 = ["Digital Collage 2012","Wolfe, Cynthia A.","Digitally created and altered collage, printed on photo paper and cut with a decorative edge.","2012","Paper","4.5\" x 6.25\"","collage (technique); mixed media;  greeting cards; digital images ","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-010"];
+
+let cwolfe_aw_011 = ["Mailart 2018","Wolfe, Cynthia A.","Altered postcard originally dating from 1930. Collaged with an image of a jungle cat, floral designs and an airplane pattern.","2018","Paper and paint", "3.25\"x5.5\"","collage (technique); mixed media;  greeting cards; postcards","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-011"];
+
+let cwolfe_aw_012 = ["Mixed Media Collage","Wolfe, Cynthia A.","Mixed media collage on a waterpaper. Composed of hand painted elements, found objects, and magazine cutouts.","circa 2015","Watercolor paper, paper, magazine, watercolor paint, metal screen, stone","4\" x 11\"","collage (technique); mixed media; found objects","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-012"];
+
+let cwolfe_aw_013 = ["At the Table","Wolfe, Cynthia A.","Abstract mixed media collage.","2020","Paper, graphite, gold leaf, paint","7.25\" x 5.25\"","collage (technique); mixed media; abstract (general art genre)","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-013"];
+
+let cwolfe_aw_014 = ["Abstract Collage Postcard","Wolfe, Cynthia A.","Hand painted earthtone shapes, cut out and collaged onto a postcard dating from 1918.","2020","Paper, ink, graphite, paint","3.25\" x 5.5\"","collage (technique); mixed media; postcards; abstract (general art genre)","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-014"];
+
+let cwolfe_aw_015 = ["Birthday Sculpture","Wolfe, Cynthia A.","3D figural wood sculpture for a 25th birthday celebration. The sculpture is meant to be hung and is reversible, revealing a unique image on both sides. The sculpture is composed of a wood base decorated with marbled paper, magazine cut outs and found metal objects. Eight bone beads (one is missing) make up the /\"limbs\" of the figure.","2013","Wood, metal, bone, paper","11.5\" x 3.5\" x 1\"","collage (technique); mixed media; birthday cards; found object sculpture","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-015"];
+
+//----------------------------variable declarations
+
+//create an array to hold works' arrays
+let works = [cwolfe_aw_001,cwolfe_aw_002,cwolfe_aw_003,cwolfe_aw_004,cwolfe_aw_005,cwolfe_aw_006,cwolfe_aw_007,cwolfe_aw_008,cwolfe_aw_009,cwolfe_aw_010,cwolfe_aw_011,cwolfe_aw_012,cwolfe_aw_013,cwolfe_aw_014,cwolfe_aw_015];
+
+//create variable for works array indexing
+let index = 0;
+
+//object to get arrays from works
+let worksIndex = works[index];
+
+//object to hold both arrays
+let theObject = {};
+
+//table variable
+let table = document.querySelector("table");
+
+//button variables
+rButton = document.getElementById("rbutton");
+lButton = document.getElementById("lbutton");
+
+//-------------------------functions to populate table at load
+//init object
+function popObject(meta, data, object){
+    meta.forEach((key, i) => object[key] = data[i]);
+    
+    // console.log(object);
+    return object;
+}
+
+//init table
+function popTable(table, objectPlz){
+    for (items in objectPlz){
+        let row = table.insertRow();
+        
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+
+        cell2.setAttribute("class","data-entry")
+        cell1.innerHTML = items;
+        cell2.innerHTML = objectPlz[items];
+    }
+}
+
+//----------------------------------button functions
+//function to change object on click
+function changeObject(data, index){
+    index = index - 1;
+
+    // newObject.values = data;
+    //if index is greater than length//
+    if (index > works.length){
+        theObject.values = data[0];
+    }
+    //else index is lower than length//
+    if (index == 0){//should this be ===?
+        theObject.values = data[14];
+    }
+    else{
+        theObject.values = data[index];
+    }
+//    set data to the upperbound
+
+    //index = 0
+    // console.log('data')
+    // console.log(data)
+    changeTableNew(data)
+}
+
+//function to change table on click
+function changeTableNew(data){
+    theTable = document.getElementsByClassName('data-entry');
+    // console.log(theTable)
+    i=0;
+    while (i < data.length){
+        theTable[i].innerHTML = data[i];
+        // console.log(data[i]);
+        i++
+    }
+}
+
+//function for button click right
+function plus(index, works){
+
+    thisIndex = index + 1;
+
+    if (index < (works.length + 1)){
+        thisIndex++
+    }
+    else{
+        thisIndex = 1;
+    }
+
+    changeObject(works[thisIndex],thisIndex,works);
+
+    document.getElementById("img").src = "cwolfe_images/cwolfe-aw-" + (thisIndex)  + ".jpg";
+    
+    //console.log(document.getElementById("img").src)
+
+    index = thisIndex - 1;
+    console.log(index);
+    return index;
+}
+
+//function for button click left
+function minus(index, works){
+    
+    thisIndex = index + 1;
+    
+    if (index > 1){
+        thisIndex = thisIndex - 1;
+    }
+    else{
+        thisIndex = works.length + 1;
+    }
+
+    changeObject(works[thisIndex],thisIndex,works);
+
+    document.getElementById("img").src = "cwolfe_images/cwolfe-aw-" + (thisIndex) + ".jpg";
+    console.log("photo #")
+    console.log(thisIndex - 1)
+
+    index = thisIndex - 1;
+    return index;
+}
+
+
+//--------------------------------------action governor
+theObject = popObject(metaCat,worksIndex,theObject);
+popTable(table,theObject);
+
+rButton.addEventListener("click", function(){
+    index = plus(index, works, metaCat)
+});
+lButton.addEventListener("click", () =>{
+    index = minus(index, works, metaCat)
+});
--- Emily_Project/script/object1.js
+++ Emily_Project/script/object1.js
@@ -1,19 +1,67 @@
 //array for metadata categories
 let metaCat = ["Title","Creator", "Description", "Date Created", "Material", "Dimensions", "Subject", "Language", "Type", "Format", "Rights", "Identifier"];
 
-//array for indv details
-let cwolfe_aw_002 = ["Valentine 2013", "Wolfe, Cynthia A", "Handmade Valentine's Day card with hand drawn flowers, foliage, and a pink and red embroidered heart on a textured paper background.", "2013", "Ink and Thread on Paper", "9\"x6\"", "collage (technique), mixed media, greeting cards, valentines", "English", "Image", "JPEG", "All rights reserved. Images may not be used without permission of the artist.", "cwolfe-aw-002"];
+//-------------------------------------arrays for indv details
+
+let cwolfe_aw_001 = ["\"Award\" Valentine 2013", "Wolfe, Cynthia A.", "Handmade paper Valentine in the shape of an award ribbon. Composed of paper, a metal ring, hemp thread , wallpaper samples, book pages and a vellum envelope" , "2013" , "Paper, Metal and Thread" ,  "6\"x3\"", "collage (technique); mixed media; greeting cards; valentines; cut-paper work" , "English" , "Image" , "JPEG" ,  "All rights reserved." , "Images may not be used without permission of the artist." , "cwolfe-aw-001"]
+
+let cwolfe_aw_002 = ["Valentine 2021", "Wolfe, Cynthia A", "Handmade Valentine's Day card with hand drawn flowers, foliage, and a pink and red embroidered heart on a textured paper background.", "2021", "Ink and Thread on Paper", "9\"x6\"", "collage (technique); mixed media; greeting cards; valentines", "English", "Image", "JPEG", "All rights reserved. Images may not be used without permission of the artist.", "cwolfe-aw-002"];
+
+let cwolfe_aw_003 = ["Valentine 2014" , "Wolfe, Cynthia A." , "Handmade paper Valentine with hand cut paper hearts and foliage with an image of a Victorian era woman on a pink glitter background. The card opens horizontally to reveal a Valentine's Day message." , "2014" , "Paper" , "5.75\" index 4.25\”" , "collage (technique); mixed media; greeting cards; valentines; cut-paper work" , "English" , "Image" , "JPEG" , "All rights reserved. Images may not be used without permission of the artist." , "cwolfe-aw-003"]
+
+let cwolfe_aw_004 = ["Valentine's Day Booklet", "Wolfe, Cynthia A.", "Handmade 14 page origami booklet decorated with hand cut hearts and abstract organic shapes. Images come from atlases, art magazines, and dictionaries. The booklet can be viewed horizontally or vertically.","circa 2015","Paper","3\"x4.5\"", "collage (technique); mixed media; greeting cards; valentines, booklets; folded books; cut-paper work", "English", "Image", "JPEG", "All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-004"]
+
+let cwolfe_aw_005 = ["Valentine 2009", "Wolfe, Cynthia A.", "Hand lettered and collaged Valentine's Day card overlayed on a page of French poetry. The main collage component is wrapped in clear cellophane and the card opens vertically to reveal a Valentine's Day message.", "2009", "Paper, Ink, Faux Gemstones, and Plastic" , "5\" index 7\"", "collage (technique); mixed media; greeting cards; valentines", "English", "Image", "JPEG", "All rights reserved. Images may not be used without permission of the artist.", "cwolfe-aw-005"]
+
+let cwolfe_aw_006 = ["Pop Up Valentine","Wolfe, Cynthia A.","Pop-up style valentine on a wallpaper sample background, decorated in painted paper cutouts.","circa 2018","Wallpaper, paint, paper, tape","10\" index 5.5\”","collage (technique); mixed media; greeting cards; valentines; pop-up books","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-006"]
+
+let cwolfe_aw_007 = ["Valentine 2020","Wolfe, Cynthia A.","Handmade collage valentine on an iridescent paper background decorated with robins and heart cut-outs. Opens vertically to reveal a valentine message.","2020","Paper, wallpaper, paper doily","5.5\" index 5\"","collage (technique); mixed media;  greeting cards; ","English","Image","JPEG","All rights reserved. Images may not be used without permission of the artist.","cwolfe-aw-007"]
+
+
+
+//----------------------------create-(dynamic)-object
 
 //create an array to hold works' arrays
-let works = [cwolfe_aw_002];
+let works = [cwolfe_aw_001,cwolfe_aw_002,cwolfe_aw_003,cwolfe_aw_004,cwolfe_aw_005,cwolfe_aw_006,cwolfe_aw_007];
+
+//create variable for works array indexing
+let index = 0;
+
+//object to get arrays from works
+let worksIndex = works[index];
+console.log(worksIndex);
 
 //create an object to hold both arrays
 let theObject = {};
 
-metaCat.forEach((key, i) => theObject[key] = cwolfe_aw_002[i]);//loop through the arrays to populate the object
-console.log(theObject)//log the object
+//create table variable
+let table = document.querySelector("table");
 
-let table = document.querySelector("table");//declare table variable
+//create variables for left and right buttons
+rButton = document.getElementById("rbutton");
+lButton = document.getElementById("lbutton");
+
+popObject(metaCat, worksIndex, table);
+
+
+//------------------populate the table----------------------
+
+//function to create object out of metaCat and worksIndex
+function popObject(meta, data){
+    meta.forEach((key, i) => data[key] = [i]);//loop through the arrays to populate the object
+    console.log(data)//log the array
+
+    //MAKE THE TABLE
+    popTable(table, data);//call the function
+}
+
+function changeObject(meta, data, table){
+    meta.forEach((key, i) => data[key] = [i]);//loop through the arrays to populate the object
+    console.log(data)//log the array
+
+    //MAKE THE TABLE
+    changeTable(table, data);//call the function
+}
 
 function popTable(table, objectPlz){//function to populate the table
     for (items in objectPlz){
@@ -26,7 +74,54 @@ function popTable(table, objectPlz){//function to populate the table
         cell1.innerHTML = items;//pop column 1 cell w/ key
         cell2.innerHTML = objectPlz[items];//populate column 2 cell w /value
     }
-
 }
 
-popTable(table, theObject)//call the function
+function changeTable(table, objectPlz){
+    for (items in objectPlz){
+        let row = table.rows[items.index].
+        console.log("items = " + row);
+        let cell1 = row.cells[0];
+        let cell2 = row.cells[1];
+
+        cell1.innerHTML = items;//pop column 1 cell w/ key
+        cell2.innerHTML = objectPlz[items];//populate column 2 cell w /value
+    }
+}
+
+//------------------arrow buttons----------------------
+
+
+//functions to iterate thru arrays
+function plus(meta, worksIndex, works, index, table){
+    //PLEASE CHANGE ME ONCE ALL ITEMS ARE IN
+    if (index < works.length){
+        index++;
+    }
+    else{
+        index = 0;
+    }
+    console.log("plus");
+    worksIndex = works[index];
+    changeObject(meta, worksIndex, table);
+    return index;
+}
+
+function minus(meta, worksIndex, works, index, table){
+    if (index > 0){
+        index--;
+    }
+    else{
+        index = 6;
+    }
+    console.log("minus");
+    worksIndex = works[index];
+    changeObject(meta, worksIndex, table);
+    return index;
+}
+
+rButton.addEventListener("click", function(){
+    plus(metaCat, worksIndex, works, index, table)
+});
+lButton.addEventListener("click", function(){
+    minus(metaCat, worksIndex, works, index, table)
+}); 
